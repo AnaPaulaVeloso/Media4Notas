382b240f10f2bf6eab9b466e74cd04850ecfacee
AnaPaulaVeloso <velosoanapaula29@gmail.com>
2021-11-02 15:35:15 -0300

    Concluir a tarefa Media4Notas

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Media4Notas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double n1, n2, n3, n4, media;
+            String resultado;
+
+            Console.WriteLine("--média--\n");
+            Console.WriteLine("Digite suas notas abaixo");
+
+            Console.Write("Nota 1: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nota 2: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nota 3: ");
+            n3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nota 4: ");
+            n4 = Convert.ToDouble(Console.ReadLine());
+
+            if (n1 < 0 || n1 > 10
+            || n2 < 0 || n2 > 10
+            || n3 < 0 || n3 > 10
+            || n4 < 0 || n4 > 10)
+            {
+                Console.WriteLine("Digite somente notas entre 0 e 10");
+            }
+            else
+            {
+                media = (n1 + n2 + n3 + n4) / 4;
+
+                if (media < 5)
+                {
+                    resultado = "Reprovado";
+                }
+                else if (media > 6)
+                {
+                    resultado = "Aprovado";
+                }
+                else
+                {
+                    resultado = "Recuperação";
+                }
+
+                Console.WriteLine($"Vocẽ ficou com média {media:N1}. resultado: {resultado}");
+            }
+
+
+        }
+    }
+}
